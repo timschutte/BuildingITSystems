de66977bfed99bedff2de882e7df1d5c1dbd9ae2
Tran Viet Hoang <95412389+Tran-Viet-Hoang@users.noreply.github.com>
2022-12-05 10:30:06 +0700

    Add files via upload

--- SchedulingBITS/SchedulingBITS/urls.py
+++ SchedulingBITS/SchedulingBITS/urls.py
@@ -1,4 +1,4 @@
-"""SchedulingBITS URL Configuration
+"""Scheduling URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name="index"),
+    path('profile',views.profile, name="profile"),
+    path('schedule',views.schedule, name="schedule"),
+    path('messages',views.messages, name="messages"),
+    path('about',views.about, name="about"),
+
 ]
-
-
