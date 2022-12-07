6cf07f1caa84547248749fb78219e1bb3ac1fd0f
timschutte <72388394+timschutte@users.noreply.github.com>
2022-12-07 13:49:22 +0700

    Fixed querying functions

--- SchedulingBITS/scheduling/models.py
+++ SchedulingBITS/scheduling/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+import sqlite3
 
 
 class User(models.Model):
@@ -15,14 +16,15 @@ class Appointment(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
     location = models.CharField(max_length=100)
     hyperlink = models.URLField()
     def __str__(self):
         return self.name
 
 class Team(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     def __str__(self):
@@ -34,13 +36,28 @@ class Membership(models.Model):
     def __str__(self):
         return self.user.name + " " + self.team.name
 
+
+
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
+
+
+def createAppointment(name, user: User, description, start_time: datetime, end_time: datetime, location, hyperlink):
+    Appointment.objects.create(name=name, user=user, description=description, start_time=int(datetime.timestamp(start_time)), end_time=int(datetime.timestamp(end_time)), location=location, hyperlink=hyperlink)
 # A function to return all times for a certain user within a certain timeframe
 # Can be used to render a calendar
-def findAppointments(start, end, user):
-    return Appointment.objects.get(f'SELECT * FROM Appointment WHERE (start_time BETWEEN {start} AND {end}) OR (end_time BETWEEN {start} AND {end}) AND user = {user};')
+def findAppointments(start_time: datetime, end_time: datetime, user: User):
+    try:
+        id = user.id
+    except AttributeError:
+        id = user
+    c.execute(f'SELECT * FROM scheduling_appointment WHERE ((start_time BETWEEN {int(start_time.timestamp())} AND {int(end_time.timestamp())}) OR (end_time BETWEEN {int(start_time.timestamp())} AND {int(end_time.timestamp())})) AND user_id = {id};')
+    return c.fetchall()
 
 # A function to return all TIMES (only the start and end time of existing appointments) for a certain team within a certain timeframe
 # Can be used for availability checking
-def returnAppointmentsTeam(start, end, team):
-    return Appointment.objects.get(f'SELCECT start_time, end_time FROM (SELECT A.start_time, A.end_time FROM Appointment A, Membership M WHERE M.user = A.user AND M.team = {team}) WHERE (start_time BETWEEN {start} AND {end}) OR (end_time BETWEEN {start} AND {end});')
-    
+def returnAppointmentsTeam(start_time: datetime, end_time: datetime, team: Team):
+    c.execute(f'SELCECT start_time, end_time FROM (SELECT A.start_time, A.end_time FROM scheduling_appointment A, scheduling_membership M WHERE M.user_id = A.user_id AND M.team_id = {team}) WHERE (start_time BETWEEN {start_time.timestamp()} AND {end_time.timestamp()}) OR (end_time BETWEEN {start_time.timestamp()} AND {end_time.timestamp()});')
+    return c.fetchall() 
+
+
