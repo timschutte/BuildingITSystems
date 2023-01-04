70d86c5a33d73adc16de913e2d94a63c039c1966
K_B <85124305+Velvetbin@users.noreply.github.com>
2023-01-04 16:20:17 +0700

    cleaning and merging minh's code

---
+++ eventcalendar/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from accounts import views
+
+app_name = "accounts"
+
+
+urlpatterns = [
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("signin/", views.SignInView.as_view(), name="signin"),
+    path("signout/", views.signout, name="signout"),
+    path("", views.SignInView.as_view(), name="signin"),
+]
